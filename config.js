4e7c230acbd8efaabb79339c2136e47d3b7c12e3
Rrusi2006 <rusithnuwantha27@gmail.com>
2025-04-29 09:29:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "mZsXzIZJ#hwNO627grKEssZeoVB3EPy_o_UZuHpBOet4nP-6IC8w",
   OWNER_NUM: process.env.OWNER_NUM || "94772136479",
-  FREFIX: process.env.PREFIX || ".",
+  PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Rrusi2006/Bot-images/refs/heads/main/20250427_201853.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now,\n\n 𝐌𝐚𝐝𝐞 𝐛𝐲 ❤️ 𝙍𝙐𝙎𝙄𝙏𝙃_𝙈𝘿_𝙑1 𝙗𝙤𝙩 😍",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
