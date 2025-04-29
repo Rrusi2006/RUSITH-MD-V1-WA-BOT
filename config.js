a21a9b7769a8099c666ab341cf3dc22870b82074
Rrusi2006 <rusithnuwantha27@gmail.com>
2025-04-29 10:59:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,7 @@ module.exports = {
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now,\n\n 𝐌𝐚𝐝𝐞 𝐛𝐲 ❤️ 𝙍𝙐𝙎𝙄𝙏𝙃_𝙈𝘿_𝙑1 𝙗𝙤𝙩 😍",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE: process.env.MODE || "public",
+  AUTO_VOICE: process.env.AUTO_VOICE || "true",
+  AUTO_STICKER: process.env.AUTO_STICKER || "true",
+  AUTO_REPLY: process.env.AUTO_REPLY || "true",
 }; 
