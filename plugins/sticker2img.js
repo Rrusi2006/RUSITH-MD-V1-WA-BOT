f967e1c4c0167e494af4a0eb1a3c058e25274424
Rrusi2006 <rusithnuwantha27@gmail.com>
2025-04-29 11:12:30 +0530

    Update sticker2img.js

--- plugins/sticker2img.js
+++ plugins/sticker2img.js
@@ -53,8 +53,8 @@ cmd(
 
       // Convert the sticker buffer to an image (using Sticker class)
       const sticker = new Sticker(stickerBuffer, {
-        pack: "𝐑_𝐎_𝐁_𝐈_𝐍",
-        author: "𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+        pack: "❤️𝙍𝙐𝙎𝙄𝙏𝙃_𝙈𝘿_𝙑1❤️",
+        author: "❤️𝙍𝙐𝙎𝙄𝙏𝙃_𝙈𝘿_𝘽𝙊𝙏❤️",
         type: "FULL", // This may not be needed, but ensures we're using the full sticker format
         quality: 100, // Quality of the output image (0-100)
       });
@@ -67,7 +67,7 @@ cmd(
         from,
         {
           image: imageBuffer,
-          caption: "Here is your converted image!\n\n𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋",
+          caption: "Here is your converted image!\n\n𝐌𝐚𝐝𝐞 𝐛𝐲 ❤️𝙍𝙐𝙎𝙄𝙏𝙃_𝙈𝘿_𝙑1❤️",
         },
         { quoted: mek }
       );
