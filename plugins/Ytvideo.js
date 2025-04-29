1245b0a191b4356da3c230589c95ff7ac330705b
Rrusi2006 <rusithnuwantha27@gmail.com>
2025-04-29 12:33:10 +0530

    Update Ytvideo.js

--- plugins/Ytvideo.js
+++ plugins/Ytvideo.js
@@ -25,7 +25,7 @@ cmd(
       const url = data.url;
 
       // Video metadata description
-      let desc = 🎥 *RUSITH_MD_VIDEO DOWNLOADER* 🎥
+      let desc = *RUSITH_MD_VIDEO DOWNLOADER*
      
 👻 *Title* : ${data.title}
 👻 *Duration* : ${data.timestamp}
